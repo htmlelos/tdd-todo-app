492e39a31322674b5f33b377b18ae7f51bab6e2b
Sergio Lucero <sergio.lucero@possumus.tech>
2023-12-02 21:44:28 -0300

    added tsconfig and vite config for vitest

--- src/App.test.ts
+++ src/App.test.ts
@@ -1,4 +1,3 @@
-import { describe, it, expect } from "vitest";
 describe("App", () => {
   it("should render", () => {
     expect(1).toBe(1);
--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div>To Do App</div>
+      <div className="App">To Do App</div>
     </>
   );
 }
